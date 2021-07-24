4d6f2151634d472a710770947ad11e11ebb7eae6
adsadilson <adilson.curso@yahoo.com.br>
2021-07-23 21:07:56 -0300

    Criação dos endpoint do recurso Funcionario

---
+++ src/main/java/br/com/apssystem/bookstore/api/resource/FuncionarioResource.java
@@ -0,0 +1,65 @@
+package br.com.apssystem.bookstore.api.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.apssystem.bookstore.api.dtos.entity.FuncionarioEntity;
+import br.com.apssystem.bookstore.api.dtos.input.FuncionarioInput;
+import br.com.apssystem.bookstore.api.dtos.mapper.FuncionarioMapper;
+import br.com.apssystem.bookstore.domain.entity.Funcionario;
+import br.com.apssystem.bookstore.domain.service.FuncionarioService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/funcionarios")
+@AllArgsConstructor
+public class FuncionarioResource {
+
+	private FuncionarioService funcionarioService;
+	private FuncionarioMapper mapper;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<FuncionarioEntity> buscarPorId(@PathVariable Long id) {
+		Funcionario obj = funcionarioService.buscarPorId(id);
+		return ResponseEntity.ok(mapper.toEntity(obj));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<FuncionarioEntity>> listarTodos() {
+		List<FuncionarioEntity> lists = mapper.toCollectionEntity(funcionarioService.listarTodos());
+		return ResponseEntity.ok(lists);
+	}
+
+	@PostMapping
+	public ResponseEntity<Funcionario> adicionar(@Valid @RequestBody Funcionario obj) {
+		obj = funcionarioService.adicionar(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<FuncionarioEntity> atualizar(@RequestBody FuncionarioInput input, @PathVariable Long id) {
+		Funcionario obj = funcionarioService.buscarPorId(id);
+		mapper.copyToDomainObject(input, obj);
+		return ResponseEntity.ok().body(mapper.toEntity(obj));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
+		funcionarioService.excluir(id);
+		return ResponseEntity.noContent().build();
+	}
+}
--- src/main/java/br/com/apssystem/bookstore/domain/service/FuncionarioService.java
+++ src/main/java/br/com/apssystem/bookstore/domain/service/FuncionarioService.java
@@ -2,8 +2,12 @@ package br.com.apssystem.bookstore.domain.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import br.com.apssystem.bookstore.api.execption.EntidadeEmUsoException;
 import br.com.apssystem.bookstore.api.execption.EntidadeNaoEncontradaException;
 import br.com.apssystem.bookstore.api.execption.NegocioException;
 import br.com.apssystem.bookstore.domain.entity.Funcionario;
@@ -31,5 +35,26 @@ public class FuncionarioService {
 				.anyMatch(func -> !func.getEmail().equals(obj.getEmail()));
 		if (result)
 			throw new NegocioException("Funcionário já cadastrado para esse [EMAIL: " + obj.getEmail() + "]");
+	}
+	
+	@Transactional
+	public Funcionario adicionar(Funcionario obj) {
+		obj.setId(null);
+		funcionarioExistente(obj);
+		return funcionarioRepository.save(obj);
+	}
+
+	@Transactional
+	public Funcionario atualizar(Funcionario obj) {
+		return adicionar(obj);
+	}
+
+	public void excluir(Long id) {
+		buscarPorId(id);
+		try {
+			funcionarioRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException("Funcionário não pode ser deletada! possui locações associados");
+		}
 	}
 }
