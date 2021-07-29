35e1cfa1b3732338689661914775635c20bb06a0
adsadilson <adilson.curso@yahoo.com.br>
2021-07-29 18:03:44 -0300

    Configuração do CrossOrigin

--- src/main/java/br/com/apssystem/bookstore/api/resource/FuncionarioResource.java
+++ src/main/java/br/com/apssystem/bookstore/api/resource/FuncionarioResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/funcionarios")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class FuncionarioResource {
 
 	private FuncionarioService funcionarioService;
--- src/main/java/br/com/apssystem/bookstore/api/resource/LivroResource.java
+++ src/main/java/br/com/apssystem/bookstore/api/resource/LivroResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/livros")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class LivroResource {
 
 	private LivroService livroService;
